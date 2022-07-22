79b25a235a1b2a962a1b2490a8d2480dde77cb6d
leejovan98 <61927063+leejovan98@users.noreply.github.com>
2022-07-22 21:38:59 +0800

    init

---
+++ src/main/java/com/jovan/sgparkingv2/SgParkingV2Application.java
@@ -0,0 +1,13 @@
+package com.jovan.sgparkingv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgParkingV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgParkingV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jovan/sgparkingv2/SgParkingV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovan.sgparkingv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgParkingV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
