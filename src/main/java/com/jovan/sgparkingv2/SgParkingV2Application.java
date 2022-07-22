7034b3ccd1b9b8e19b993350590e93299a5835cb
leejovan98 <61927063+leejovan98@users.noreply.github.com>
2022-07-22 23:41:07 +0800

    added initial flyway scripts and dev properties

--- src/main/java/com/jovan/sgparkingv2/SgParkingV2Application.java
+++ src/main/java/com/jovan/sgparkingv2/SgParkingV2Application.java
@@ -1,9 +1,11 @@
 package com.jovan.sgparkingv2;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SgParkingV2Application {
 
 	public static void main(String[] args) {
---
+++ src/main/resources/db/migration/V1__initial_schema_setup.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS carpark_details(
+   carpark_number         VARCHAR(10) NOT NULL PRIMARY KEY,
+   address                VARCHAR(100) NOT NULL,
+   x_coord                NUMERIC(10,4) NOT NULL,
+   y_coord                NUMERIC(10,4) NOT NULL,
+   carpark_type           VARCHAR(50) NOT NULL,
+   type_of_parking_system VARCHAR(30) NOT NULL,
+   short_term_parking     VARCHAR(20) NOT NULL,
+   free_parking           VARCHAR(30) NOT NULL,
+   night_parking          VARCHAR(5) NOT NULL,
+   car_park_decks         INTEGER  NOT NULL,
+   gantry_height          NUMERIC(4,2) NOT NULL,
+   carpark_basement       VARCHAR(1) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS carpark_availability(
+   carpark_number         VARCHAR(10) NOT NULL PRIMARY KEY,
+   lot_type               VARCHAR(5) NOT NULL,
+   total_lots             INTEGER NOT NULL,
+   lots_available         INTEGER NOT NULL,
+   update_datetime        TIMESTAMP NOT NULL
+);
