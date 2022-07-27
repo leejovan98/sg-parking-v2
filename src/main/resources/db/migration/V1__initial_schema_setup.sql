84a983b53b75437265dfa3665e4d323b7beaf9cb
Jovan <leejovan98_dv@outlook.sg>
2022-07-27 10:00:39 +0800

    modified schema flyway

--- src/main/resources/db/migration/V1__initial_schema_setup.sql
+++ src/main/resources/db/migration/V1__initial_schema_setup.sql
@@ -8,16 +8,17 @@ CREATE TABLE IF NOT EXISTS carpark_details(
    short_term_parking     VARCHAR(20) NOT NULL,
    free_parking           VARCHAR(30) NOT NULL,
    night_parking          VARCHAR(5) NOT NULL,
-   carpark_decks         INTEGER  NOT NULL,
+   carpark_decks          INTEGER  NOT NULL,
    gantry_height          NUMERIC(4,2) NOT NULL,
    carpark_basement       VARCHAR(1) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS carpark_availability(
-   carpark_number         VARCHAR(10) NOT NULL PRIMARY KEY,
+   carpark_number         VARCHAR(10) NOT NULL,
    lot_type               VARCHAR(5) NOT NULL,
    total_lots             INTEGER NOT NULL,
    lots_available         INTEGER NOT NULL,
-   update_datetime        TIMESTAMP NOT NULL
+   update_datetime        TIMESTAMPTZ NOT NULL,
+   PRIMARY KEY(carpark_number, lot_type)
 );
